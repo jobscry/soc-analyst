4f55954a3650296d20f88f0f806491e0802187ea
joe vasquez <joe.vasquez@redwatchai.com>
2019-07-13 22:48:10 -0500

    added fixture for superuser

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,6 +2,7 @@ import pytest
 from falcon import testing
 
 from analyst.app import AnalystService
+from analyst.models.user import create_user
 
 
 class TestDBConfig:
@@ -14,9 +15,15 @@ class TestConfig:
         self.db = TestDBConfig()
         self.asn_path = "./etc/analyst/GeoLite2-ASN/GeoLite2-ASN.mmdb"
         self.geo_path = "./etc/analyst/GeoLite2-City/GeoLite2-City.mmdb"
-        self.version = "1"
+        self.version = "test"
 
 
 @pytest.fixture()
 def client():
     return testing.TestClient(AnalystService(cfg=TestConfig()))
+
+
+@pytest.fixture()
+def superuser(client):
+    u = create_user(username="superuser", password="password", is_admin=True)
+    return u
