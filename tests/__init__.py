8cb28124e77e6e9a3f6e2c4d8352a982cc62254d
joe vasquez <joe.vasquez@redwatchai.com>
2019-07-13 20:02:55 -0500

    added configuration to test client fixture

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,15 +3,20 @@ from falcon import testing
 
 from analyst.app import AnalystService
 
-TEST_CONFIG = {
-    'db': {
-        'file_path': ':memory:'
-    },
-    'asn_path': './etc/analyst/GeoLite2-ASN/GeoLite2-ASN.mmdb',
-    'geo_path': './etc/analyst/GeoLite2-City/GeoLite2-City.mmdb'
-}
+
+class TestDBConfig:
+    def __init__(self):
+        self.file_path = ":memory:"
+
+
+class TestConfig:
+    def __init__(self):
+        self.db = TestDBConfig()
+        self.asn_path = "./etc/analyst/GeoLite2-ASN/GeoLite2-ASN.mmdb"
+        self.geo_path = "./etc/analyst/GeoLite2-City/GeoLite2-City.mmdb"
+        self.version = "1"
 
 
 @pytest.fixture()
 def client():
-    return testing.TestClient(AnalystService(cfg=TEST_CONFIG))
+    return testing.TestClient(AnalystService(cfg=TestConfig()))
